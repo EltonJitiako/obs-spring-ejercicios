86c47a23b40141eec00189945fcb60e91fc5c1ce
Elton Jitiako <eltonjitiako@gmail.com>
2022-12-26 11:50:54 +0100

    1 commit

---
+++ ejercicio1/src/main/java/org/example/Saludo.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Saludo {
+
+    //Constructor
+    public Saludo() {
+    }
+
+    //Method
+    public void imprimirSaludo() {
+
+        System.out.println("Imprimindo saludo: Hola!!!");
+
+    }
+
+}
